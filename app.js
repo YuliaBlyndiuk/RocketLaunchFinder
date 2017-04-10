dc0683935eebf1faa72a0a3627814d0e9eaa9d27
Yulia Blyndiuk <yulia.blyndiuk@gmail.com>
2017-04-10 11:24:07 -0400

    date fix
    make the date of request work

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var endPoint = 'https://launchlibrary.net/1.2/launch/2015-08-20';
+//getting the the date of the request date:
+var now = new Date();
+var dateNow = now.getDate();
+var monthNow = now.getMonth() + 1;
+var yearNow = now.getFullYear();
+
+var endPoint = 'https://launchlibrary.net/1.2/launch/' + yearNow + '-0' + monthNow + '-' + dateNow;
 
 var state = {
 	display: true
@@ -8,22 +14,50 @@ function changeClass(){
 	state.display = false;
 }
 
+function getLaunchData(searchTerm, callback){
+	$.ajax({
+		url: endPoint,
+		type: 'GET',
+		// data: {
+		// 	"name": "",
+		// 	"windowstart": "",
+		// 	"windowend": "",
+		// 	"vidURLs": [],
+		// 	"location": "",
+		// 	"missions": [{
+		// 		"description": ""
+		// 	}]
+		// },
+		dataType: 'json',
+		success: function(data){
+			console.log('data' + data);
+		}
+	});
+}
+
 function render(){
 	if (state.display === false) {
 		$('#images, #buttons').css("display", "none");
 	} 
 }
 
-//listener: when img is clicked...
 function listenerWatcher(){
 	$('#images').on('click', 'div', function(event){
 		console.log(event.target, event.currentTarget);
 		changeClass();
 		render();
+		getLaunchData();
+
+		// getLaunchData will need to be async
+		// and take a callback. (You will use
+		// jQuery $.get or $.ajax methods).
+		//
+		// It will put data into the state.
+		// Then render.
+		//
 	});
 }
 
-//when the page loads...
 $(function(){
 	listenerWatcher();
 });
