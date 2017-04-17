eb11852506ad6ea85e8fa77f207fe630a93d5277
Yulia Blyndiuk <yulia.blyndiuk@gmail.com>
2017-04-17 19:29:32 -0400

    Update app.js
    Add design to the result page

--- app.js
+++ app.js
@@ -27,13 +27,24 @@ function changeClass(){
 }
 
 // 8) getting data from the server and displaying it
-function getData(){
+function getData1(){
 	$.ajax({
 		url: endPoint,
 		type: 'GET',
 		success: function(result){
 			addDataToState(state, result);
-			displayData(state);
+			displayData17(state);
+		}
+	})
+}
+
+function getData2(){
+	$.ajax({
+		url: endPoint,
+		type: 'GET',
+		success: function(result){
+			addDataToState(state, result);
+			displayData16(state);
 		}
 	})
 }
@@ -41,28 +52,55 @@ function getData(){
 // 9) adding received data to the state
 function addDataToState(state, result){
 	state.launchData.push(result);
+	console.log('state is', state);
 }
 
 //changing the state to get necessary information out of it
-function displayData(state){
+function displayData16(state){
 	var Schedule16 = "";
-	
 
-	//console.log('state is', state);
+	// var header1 = '<h1 class='header1'>Schedule Of Rocket Launches at Kennedy Space Center</h1>';
+
 	for (var i = 0; i < state.launchData[0].launches.length; i++) {
 		if (state.launchData[0].launches[i].location.id === 16) {
 
+		var rocketImg = state.launchData[0].launches[i].rocket.imageURL;
+		var rocketName = state.launchData[0].launches[i].rocket.name;
 		var missionName = state.launchData[0].launches[i].missions[0].name;
 		var missionDescription = state.launchData[0].launches[i].missions[0].description;
 		var windowStart = state.launchData[0].launches[i].windowstart;
 		var windowEnd = state.launchData[0].launches[i].windowend;
 
 			state.launchData.forEach(function(item){
-			Schedule16 += '<div>Mission Name: '+ missionName + '</div><div>Mission Description:' + missionDescription + '</div><div>Opening of Launch Window: ' + windowStart + '</div><div>Closing of Launch Window: ' + windowEnd + '</div>';
-			$("#app").html(Schedule16);
+			Schedule16 += '<div class="scheduleBlock"><div class="rocketImageBox"><img class="rocketImage" src="' + rocketImg + '"></div><b>Rocket name:</b> ' + rocketName + '<br><b>Mission Name:</b> '+ missionName + '<br><b>Mission Description:</b> ' + missionDescription + '<br><b>Opening of Launch Window:</b> ' + windowStart + '<br><b>Closing of Launch Window:</b> ' + windowEnd + '</div>';
+
+			$("#app").append(Schedule16);
 			})
-		}
-		
+		}	
+	}
+}
+
+function displayData17(state){
+	var Schedule17 = "";
+
+	// var header2 = '<h1 class='header1'>Schedule Of Rocket Launches at Cape Canaveral Air Force Station</h1>';
+
+	for (var i = 0; i < state.launchData[0].launches.length; i++) {
+		if (state.launchData[0].launches[i].location.id === 17) {
+
+		var rocketImg = state.launchData[0].launches[i].rocket.imageURL;
+		var rocketName = state.launchData[0].launches[i].rocket.name;
+		var missionName = state.launchData[0].launches[i].missions[0].name;
+		var missionDescription = state.launchData[0].launches[i].missions[0].description;
+		var windowStart = state.launchData[0].launches[i].windowstart;
+		var windowEnd = state.launchData[0].launches[i].windowend;
+
+			state.launchData.forEach(function(item){
+			Schedule17 += '<div class="scheduleBlock"><div class="rocketImageBox"><img class="rocketImage" src="' + rocketImg + '"></div><b>Rocket name:</b> ' + rocketName + '<br><b>Mission Name:</b> '+ missionName + '<br><b>Mission Description:</b> ' + missionDescription + '<br><b>Opening of Launch Window:</b> ' + windowStart + '<br><b>Closing of Launch Window:</b> ' + windowEnd + '</div>';
+
+			$("#app").append(Schedule17);
+			})
+		}	
 	}
 }
 
@@ -74,14 +112,21 @@ function render(){
 
 }
 
-// 2) it listens to a user action (click) and reacts to it
 function listenerWatcher(){
-	$('#images').on('click', 'div', function(event){
+	$('#img1').on('click', function(event){
 		changeClass();
 		render();
-		getData();
-		
-	});
+		getData1();
+
+	})
+
+	$('#img2').on('click', function(event){
+		changeClass();
+		render();
+		getData2();
+
+	})
+
 }
 
 // 1) once the page is loaded..
